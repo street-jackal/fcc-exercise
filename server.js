9e40cbe48e3a16a5f1beb8fc7bcc58da8d1cff3d
street-jackal <jon.mcintire.myers@gmail.com>
2021-01-02 16:15:14 -0800

    hopefully fixed response display

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/api/exercise/log', (request, response) => {
       if (request.query.limit) {
         responseObject.log = responseObject.log.slice(0, request.query.limit)
       }
-
+      
       responseObject['count'] = result.log.length;
       //response.json(responseObject);
       document.getElementById("jsonoutput").textContent = JSON.stringify(responseObject);
